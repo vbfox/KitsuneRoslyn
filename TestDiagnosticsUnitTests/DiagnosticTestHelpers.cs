bb6300c1d7879c5f84f40fdeda653981c37ee0e3
Julien Roncaglia <julien@roncaglia.fr>
2014-07-02 22:05:17 +0200

    Add basic unit tests

---
+++ TestDiagnosticsUnitTests/DiagnosticTestHelpers.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+
+namespace TestDiagnosticsUnitTests
+{
+    static class DiagnosticTestHelpers
+    {
+        public static ImmutableList<Diagnostic> GetDiagnosticsInSimpleCode(
+            ISyntaxNodeAnalyzer<SyntaxKind> analyzer, string code)
+        {
+            var fullCode = string.Format("using System; namespace TestNamespace {{ public class TestClass {{ "
+                + "public static void TestMethod() {{ {0} }} }} }}", code);
+
+            return GetDiagnostics(analyzer, fullCode);
+        }
+
+        public static ImmutableList<Diagnostic> GetDiagnostics(
+            ISyntaxNodeAnalyzer<SyntaxKind> analyzer, string code)
+        {
+            var tree = SyntaxFactory.ParseSyntaxTree(code);
+            var compilation = CSharpCompilation.Create(null,
+                syntaxTrees: ImmutableArray.Create(tree),
+                references: new[]
+                {
+                    new MetadataFileReference(typeof(object).Assembly.Location),
+                });
+            var root = tree.GetRoot();
+
+            var kinds = analyzer.SyntaxKindsOfInterest;
+
+            var matchingNodes =
+                from node in RecursiveGetChild(tree.GetRoot())
+                where kinds.Any(k => node.CSharpKind() == k)
+                select node;
+
+            var diagnostics = ImmutableList<Diagnostic>.Empty;
+
+            foreach (var node in matchingNodes)
+            {
+                analyzer.AnalyzeNode(
+                    node,
+                    compilation.GetSemanticModel(tree),
+                    d => diagnostics = diagnostics.Add(d),
+                    CancellationToken.None);
+            }
+
+            return diagnostics;
+        }
+
+        private static IEnumerable<SyntaxNode> RecursiveGetChild(SyntaxNode node)
+        {
+            if (node == null)
+            {
+                throw new ArgumentNullException("node");
+            }
+
+            Stack<SyntaxNode> x = new Stack<SyntaxNode>();
+            x.Push(node);
+
+            while (x.Count != 0)
+            {
+                var currentNode = x.Pop();
+
+                yield return currentNode;
+
+                foreach (var childNode in currentNode.ChildNodes())
+                {
+                    x.Push(childNode);
+                }
+            }
+        }
+    }
+}
---
+++ TestDiagnosticsUnitTests/NoStringEmptyDiagnosticTests.cs
@@ -0,0 +1,42 @@
+﻿using BlackFox.Roslyn.TestDiagnostics.NoStringEmpty;
+using NFluent;
+using Xunit;
+using TestDiagnosticsUnitTests.DiagnosticTestHelpers;
+
+namespace TestDiagnosticsUnitTests
+{
+    public class NoStringEmptyDiagnosticTests
+    {
+        [Fact]
+        public void No_diagnostic_on_empty_string()
+        {
+            var analyzer = new NoStringEmptyDiagnostic();
+            var diagnostics = GetDiagnosticsInSimpleCode(analyzer, "var x = \"\";");
+            Check.That(diagnostics).IsEmpty();
+        }
+
+        [Fact]
+        public void Diagnostic_on_standard_call()
+        {
+            var analyzer = new NoStringEmptyDiagnostic();
+            var diagnostics = GetDiagnosticsInSimpleCode(analyzer, "var x = String.Empty;");
+            Check.That(diagnostics).HasSize(1);
+        }
+
+        [Fact]
+        public void Diagnostic_on_namespace_qualified_call()
+        {
+            var analyzer = new NoStringEmptyDiagnostic();
+            var diagnostics = GetDiagnosticsInSimpleCode(analyzer, "var x = System.String.Empty;");
+            Check.That(diagnostics).HasSize(1);
+        }
+
+        [Fact]
+        public void Diagnostic_on_fully_qualified_call()
+        {
+            var analyzer = new NoStringEmptyDiagnostic();
+            var diagnostics = GetDiagnosticsInSimpleCode(analyzer, "var x = global::System.String.Empty;");
+            Check.That(diagnostics).HasSize(1);
+        }
+    }
+}
