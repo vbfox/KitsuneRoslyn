131aba34938ecf3953a2224f901e0a165beb37e4
Julien Roncaglia <julien@roncaglia.fr>
2014-07-04 22:38:59 +0200

    Add tests for NoNewGuidAnalyzer

---
+++ TestDiagnosticsUnitTests/NoNewGuidAnalyzerTests.cs
@@ -0,0 +1,44 @@
+﻿using BlackFox.Roslyn.TestDiagnostics.NoNewGuid;
+using BlackFox.Roslyn.TestDiagnostics.NoStringEmpty;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NFluent;
+using TestDiagnosticsUnitTests.Helpers.DiagnosticTestHelpers;
+
+namespace TestDiagnosticsUnitTests
+{
+    [TestClass]
+    public class NoNewGuidAnalyzerTests
+    {
+        [TestMethod]
+        public void No_diagnostic_on_guid_empty()
+        {
+            var analyzer = new NoNewGuidAnalyzer();
+            var diagnostics = GetDiagnosticsInSimpleCode(analyzer, "var x = Guid.Empty;");
+            Check.That(diagnostics).IsEmpty();
+        }
+
+        [TestMethod]
+        public void Diagnostic_new_call()
+        {
+            var analyzer = new NoNewGuidAnalyzer();
+            var diagnostics = GetDiagnosticsInSimpleCode(analyzer, "var x = new Guid();");
+            Check.That(diagnostics).HasSize(1);
+        }
+
+        [TestMethod]
+        public void Diagnostic_on_namespace_qualified_call()
+        {
+            var analyzer = new NoNewGuidAnalyzer();
+            var diagnostics = GetDiagnosticsInSimpleCode(analyzer, "var x = new System.Guid();");
+            Check.That(diagnostics).HasSize(1);
+        }
+
+        [TestMethod]
+        public void Diagnostic_on_fully_qualified_call()
+        {
+            var analyzer = new NoNewGuidAnalyzer();
+            var diagnostics = GetDiagnosticsInSimpleCode(analyzer, "var x = new global::System.Guid();");
+            Check.That(diagnostics).HasSize(1);
+        }
+    }
+}
