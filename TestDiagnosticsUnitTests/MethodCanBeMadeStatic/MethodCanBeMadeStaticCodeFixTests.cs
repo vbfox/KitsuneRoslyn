1b49722bde7b48fd5ba7226d43b011d375508e06
Julien Roncaglia <julien@roncaglia.fr>
2014-08-01 22:36:41 +0200

    Remove useless usings

--- TestDiagnosticsUnitTests/MethodCanBeMadeStatic/MethodCanBeMadeStaticCodeFixTests.cs
+++ TestDiagnosticsUnitTests/MethodCanBeMadeStatic/MethodCanBeMadeStaticCodeFixTests.cs
@@ -1,10 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BlackFox.Roslyn.Diagnostics.TestHelpers.CodeFixTestHelpers;
+﻿using BlackFox.Roslyn.Diagnostics.TestHelpers.CodeFixTestHelpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BlackFox.Roslyn.Diagnostics.MethodCanBeMadeStatic
 {
