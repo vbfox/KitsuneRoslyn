471e64682e9afef44cbed9102c511bbfde83769e
Julien Roncaglia <julien@roncaglia.fr>
2014-08-01 15:55:08 +0200

    More unit tests for property analyzer

--- TestDiagnosticsUnitTests/PropertyConversions/PropertyAnalyzerTests.cs
+++ TestDiagnosticsUnitTests/PropertyConversions/PropertyAnalyzerTests.cs
@@ -17,25 +17,27 @@ namespace BlackFox.Roslyn.Diagnostics.PropertyConversions
     public class PropertyAnalyzerTests
     {
         [TestMethod]
-        public void Property_return_constant()
+        public void Get_return_constant()
         {
             var analyzer = new PropertyAnalyzer();
             var diagnostics = GetDiagnosticsInClassLevelCode(analyzer, "public int X { get { return 42; } }");
             var ids = diagnostics.Select(d => d.Id);
-            Check.That(ids).IsOnlyMadeOf(PropertyAnalyzer.IdToExpression, PropertyAnalyzer.IdToInitializer);
+            Check.That(ids).IsOnlyMadeOf(PropertyAnalyzer.IdToExpression, PropertyAnalyzer.IdToInitializer)
+                .And.Not.IsEmpty();
         }
 
         [TestMethod]
-        public void Property_return_expression()
+        public void Get_return_expression()
         {
             var analyzer = new PropertyAnalyzer();
             var diagnostics = GetDiagnosticsInClassLevelCode(analyzer, "public int X { get { return Math.Sin(42); } }");
             var ids = diagnostics.Select(d => d.Id);
-            Check.That(ids).IsOnlyMadeOf(PropertyAnalyzer.IdToExpression);
+            Check.That(ids).IsOnlyMadeOf(PropertyAnalyzer.IdToExpression)
+                .And.Not.IsEmpty();
         }
 
         [TestMethod]
-        public void Property_get_set_auto_implemented()
+        public void Get_set_auto_implemented()
         {
             var analyzer = new PropertyAnalyzer();
             var diagnostics = GetDiagnosticsInClassLevelCode(analyzer, "public int X { get; set; }");
@@ -43,11 +45,78 @@ namespace BlackFox.Roslyn.Diagnostics.PropertyConversions
         }
 
         [TestMethod]
-        public void Property_get_set()
+        public void Get_set()
         {
             var analyzer = new PropertyAnalyzer();
             var diagnostics = GetDiagnosticsInClassLevelCode(analyzer, "public int X { get { return 42; } set {} }");
             Check.That(diagnostics).IsEmpty();
+        }
+
+        [TestMethod]
+        public void Expression_constant()
+        {
+            var analyzer = new PropertyAnalyzer();
+            var diagnostics = GetDiagnosticsInClassLevelCode(analyzer, "public int X => 42;");
+            var ids = diagnostics.Select(d => d.Id);
+            Check.That(ids).IsOnlyMadeOf(PropertyAnalyzer.IdToStatement, PropertyAnalyzer.IdToInitializer)
+                .And.Not.IsEmpty();
+        }
+
+        [TestMethod]
+        public void Expression_expression_static_1()
+        {
+            var analyzer = new PropertyAnalyzer();
+            var diagnostics = GetDiagnosticsInClassLevelCode(analyzer, "public int X => Math.Sin(42);");
+            var ids = diagnostics.Select(d => d.Id);
+            Check.That(ids).IsOnlyMadeOf(PropertyAnalyzer.IdToStatement, PropertyAnalyzer.IdToInitializer)
+                .And.Not.IsEmpty();
+        }
+
+        [TestMethod]
+        public void Expression_expression_static_2()
+        {
+            var analyzer = new PropertyAnalyzer();
+            var diagnostics = GetDiagnosticsInClassLevelCode(analyzer,
+                "public static int x = 42; public int SinX => Math.Sin(x);");
+            var ids = diagnostics.Select(d => d.Id);
+            Check.That(ids).IsOnlyMadeOf(PropertyAnalyzer.IdToStatement, PropertyAnalyzer.IdToInitializer)
+                .And.Not.IsEmpty();
+        }
+
+        [TestMethod]
+        public void Expression_expression_instance()
+        {
+            var analyzer = new PropertyAnalyzer();
+            var diagnostics = GetDiagnosticsInClassLevelCode(analyzer,
+                "public int x = 42; public int SinX => Math.Sin(x);");
+            var ids = diagnostics.Select(d => d.Id);
+
+            // Intializer is impossible as no instance reference can exist in their scope
+            Check.That(ids).IsOnlyMadeOf(PropertyAnalyzer.IdToStatement)
+                .And.Not.IsEmpty();
+        }
+
+        public double X { get; } = Math.Sin(42);
+
+        [TestMethod]
+        public void Initializer_constant()
+        {
+            var analyzer = new PropertyAnalyzer();
+            var diagnostics = GetDiagnosticsInClassLevelCode(analyzer, "public int X {get;} = 42;");
+            var ids = diagnostics.Select(d => d.Id);
+            Check.That(ids).IsOnlyMadeOf(PropertyAnalyzer.IdToStatement, PropertyAnalyzer.IdToExpression)
+                .And.Not.IsEmpty();
+        }
+
+        [TestMethod]
+        public void Initializer_expression()
+        {
+            var analyzer = new PropertyAnalyzer();
+            var diagnostics = GetDiagnosticsInClassLevelCode(analyzer,
+                "public int X {get;} = Math.Sin(42);");
+            var ids = diagnostics.Select(d => d.Id);
+            Check.That(ids).IsOnlyMadeOf(PropertyAnalyzer.IdToStatement, PropertyAnalyzer.IdToExpression)
+                .And.Not.IsEmpty();
         }
     }
 }
