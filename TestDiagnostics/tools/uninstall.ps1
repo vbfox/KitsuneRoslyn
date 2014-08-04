4e2d542a2cb147b6bec622f7e018a2e507d297d8
Julien Roncaglia <julien@roncaglia.fr>
2014-08-04 22:26:17 +0200

    Correct NuGet dll names

--- TestDiagnostics/tools/uninstall.ps1
+++ TestDiagnostics/tools/uninstall.ps1
@@ -3,6 +3,6 @@ param($installPath, $toolsPath, $package, $project)
 $p = Get-Project
 
 $analyzerPath = join-path $toolsPath "analyzers"
-$analyzerFilePath = join-path $analyzerPath "Diagnostic1.dll"
+$analyzerFilePath = join-path $analyzerPath "BlackFox.Roslyn.Diagnostics.dll"
 
 $p.Object.AnalyzerReferences.Remove("$analyzerFilePath")
